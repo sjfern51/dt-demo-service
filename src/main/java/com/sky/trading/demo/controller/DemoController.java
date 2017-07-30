3d4c13b74405f19f16b8a01b216cc9cf405f04b2
Simon Fern <simon.fern@sky.uk>
2017-07-30 18:56:27 +0100

    feature: add service

--- src/main/java/com/sky/trading/demo/controller/DemoController.java
+++ src/main/java/com/sky/trading/demo/controller/DemoController.java
@@ -1,7 +1,9 @@
 package com.sky.trading.demo.controller;
 
 import com.sky.trading.demo.model.Message;
+import com.sky.trading.demo.service.DemoService;
 import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,13 @@ import java.util.concurrent.CompletableFuture;
 @RequestMapping(path = "/demo", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DemoController {
 
+    private final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
     @ApiOperation("Demo operation")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "message", value = "Message to process", paramType = "path")
@@ -21,6 +30,6 @@ public class DemoController {
     })
     @GetMapping("/{message}")
     public CompletableFuture<Message> getMessage(@PathVariable String message) {
-        return CompletableFuture.supplyAsync(() -> new Message(message));
+        return demoService.getMessage(message);
     }
 }
--- src/test/java/com/sky/trading/demo/controller/DemoControllerTest.java
+++ src/test/java/com/sky/trading/demo/controller/DemoControllerTest.java
@@ -1,14 +1,20 @@
 package com.sky.trading.demo.controller;
 
+import com.sky.trading.demo.model.Message;
+import com.sky.trading.demo.service.DemoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.concurrent.CompletableFuture;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -21,8 +27,14 @@ public class DemoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private DemoService demoService;
+
     @Test
     public void get_returnsDemo() throws Exception {
+        when(demoService.getMessage("test"))
+                .thenReturn(CompletableFuture.completedFuture(new Message("test")));
+
         MvcResult mvcResult = mockMvc.perform(get("/demo/test"))
                 .andExpect(request().asyncStarted())
                 .andReturn();
